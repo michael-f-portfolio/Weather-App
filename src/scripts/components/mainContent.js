e8fa442e5306334559c9825f7001b791a3cab53f
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2024-01-09 16:52:42 -0700

    created display container for result of getWeather call

--- src/scripts/components/mainContent.js
+++ src/scripts/components/mainContent.js
@@ -1,4 +1,5 @@
 import "styles/mainContent.css";
+import SearchWeatherForm from "./searchWeatherForm";
 
 export default class MainContent {
   constructor(parentElement, content) {
@@ -7,36 +8,83 @@ export default class MainContent {
     this.mainContentContainer = document.createElement("div");
     this.mainContentContainer.id = "main-content-container";
 
-    this.contentDisplay = document.createElement("h2");
-    this.contentDisplay.textContent = content;
+    this.weatherDisplayContainer = document.createElement("div");
+    this.weatherDisplayContainer.id = "weather-display-container";
 
-    this.mainContentContainer.appendChild(this.contentDisplay);
+    this.locationContainer = document.createElement("div");
+    this.locationContainer.id = "location-container";
 
-    this.form = document.createElement("form");
-    this.form.id = "main-model-form";
+    this.cityDisplay = document.createElement("div");
+    this.provinceDisplay = document.createElement("div");
+    this.countryDisplay = document.createElement("div");
+    this.locationContainer.append(
+      this.cityDisplay,
+      this.provinceDisplay,
+      this.countryDisplay
+    );
 
-    this.textInput = document.createElement("input");
-    this.textInput.type = "text";
-    this.textInput.placeholder = "Update Main Content...";
+    this.temperatureContainer = document.createElement("div");
+    this.temperatureContainer.id = "temperature-container";
 
-    this.formSubmitButton = document.createElement("button");
-    this.formSubmitButton.type = "submit";
-    this.formSubmitButton.textContent = "Submit";
+    this.currentConditionContainer = document.createElement("div");
+    this.currentConditionContainer.id = "current-conditions-container";
+    this.currentConditionIconDisplay = document.createElement("img");
+    this.currentConditionDisplay = document.createElement("div");
+    this.currentConditionContainer.append(
+      this.currentConditionIconDisplay,
+      this.currentConditionDisplay
+    );
+    this.temperatureDisplay = document.createElement("div");
+    this.temperatureDisplay.id = "temperature";
+    this.temperatureContainer.append(
+      this.currentConditionContainer,
+      this.temperatureDisplay
+    );
 
-    this.form.append(this.textInput, this.formSubmitButton);
-    this.mainContentContainer.appendChild(this.form);
+    this.windConditionsContainer = document.createElement("div");
+    this.windConditionsContainer.id = "wind-conditions-container";
+    this.windSpeedDisplay = document.createElement("div");
+    this.windDirectionDisplay = document.createElement("div");
+    this.windConditionsContainer.append(
+      this.windSpeedDisplay,
+      this.windDirectionDisplay
+    );
+    this.weatherDisplayContainer.append(
+      this.locationContainer,
+      this.temperatureContainer,
+      this.windConditionsContainer
+    );
+
+    this.messageDisplay = document.createElement("h2");
+    this.messageDisplay.textContent = content;
+
+    this.mainContentContainer.append(
+      this.messageDisplay,
+      this.weatherDisplayContainer
+    );
+
+    this.searchWeatherForm = new SearchWeatherForm(this.mainContentContainer);
 
     this.parent.appendChild(this.mainContentContainer);
   }
 
   displayContent(content) {
-    this.contentDisplay.textContent = content;
-  }
-
-  bindUpdateContent(handler) {
-    this.formSubmitButton.addEventListener("click", (event) => {
-      event.preventDefault();
-      handler(this.textInput.value);
-    });
+    if (content.error) {
+      this.messageDisplay.textContent = content.error;
+    } else {
+      this.messageDisplay.textContent = "";
+      this.cityDisplay.textContent = `${content.city}, `;
+      this.provinceDisplay.textContent = `${content.province}, `;
+      this.countryDisplay.textContent = content.country;
+      this.currentConditionIconDisplay.src = content.currentConditionIcon;
+      this.currentConditionDisplay.textContent = content.currentCondition;
+      this.temperatureDisplay.textContent = `${content.temperature} ${
+        content.asCelsius ? "°C" : "°F"
+      }`;
+      this.windSpeedDisplay.textContent = `${content.windSpeed} ${
+        content.asCelsius ? "KPH" : "MPH"
+      }`;
+      this.windDirectionDisplay.textContent = `(${content.windDirection})`;
+    }
   }
 }
