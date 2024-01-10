b1a2f271e0ba7ec669d0b97c410eb4c17840712d
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2024-01-09 18:41:16 -0700

    added responsive styling

--- src/scripts/components/mainContent.js
+++ src/scripts/components/mainContent.js
@@ -6,7 +6,11 @@ export default class MainContent {
     this.parent = parentElement;
 
     this.mainContentContainer = document.createElement("div");
-    this.mainContentContainer.id = "main-content-container";
+    this.mainContentContainer.id = "main-container";
+
+    this.messageDisplay = document.createElement("h2");
+    this.messageDisplay.id = "message-display";
+    this.messageDisplay.textContent = content;
 
     this.weatherDisplayContainer = document.createElement("div");
     this.weatherDisplayContainer.id = "weather-display-container";
@@ -16,8 +20,11 @@ export default class MainContent {
     this.locationContainer.id = "location-container";
 
     this.cityDisplay = document.createElement("div");
+    this.cityDisplay.id = "city-display";
     this.provinceDisplay = document.createElement("div");
+    this.provinceDisplay.id = "province-display";
     this.countryDisplay = document.createElement("div");
+    this.countryDisplay.id = "country-display";
     this.locationContainer.append(
       this.cityDisplay,
       this.provinceDisplay,
@@ -28,24 +35,28 @@ export default class MainContent {
     this.temperatureContainer.id = "temperature-container";
 
     this.currentConditionContainer = document.createElement("div");
-    this.currentConditionContainer.id = "current-conditions-container";
+    this.currentConditionContainer.id = "current-condition-container";
     this.currentConditionIconDisplay = document.createElement("img");
+    this.currentConditionIconDisplay.id = "current-condition-icon";
     this.currentConditionDisplay = document.createElement("div");
+    this.currentConditionDisplay.id = "current-condition-display";
     this.currentConditionContainer.append(
       this.currentConditionIconDisplay,
       this.currentConditionDisplay
     );
     this.temperatureDisplay = document.createElement("div");
-    this.temperatureDisplay.id = "temperature";
+    this.temperatureDisplay.id = "temperature-display";
     this.temperatureContainer.append(
       this.currentConditionContainer,
       this.temperatureDisplay
     );
 
     this.windConditionsContainer = document.createElement("div");
-    this.windConditionsContainer.id = "wind-conditions-container";
+    this.windConditionsContainer.id = "wind-condition-container";
     this.windSpeedDisplay = document.createElement("div");
+    this.windSpeedDisplay.id = "wind-speed-display";
     this.windDirectionDisplay = document.createElement("div");
+    this.windDirectionDisplay.id = "wind-direction-display";
     this.windConditionsContainer.append(
       this.windSpeedDisplay,
       this.windDirectionDisplay
@@ -55,9 +66,6 @@ export default class MainContent {
       this.temperatureContainer,
       this.windConditionsContainer
     );
-
-    this.messageDisplay = document.createElement("h2");
-    this.messageDisplay.textContent = content;
 
     this.mainContentContainer.append(
       this.messageDisplay,
@@ -75,8 +83,10 @@ export default class MainContent {
     } else {
       this.weatherDisplayContainer.classList.remove("hidden");
       this.messageDisplay.textContent = "";
-      this.cityDisplay.textContent = `${content.city}, `;
-      this.provinceDisplay.textContent = `${content.province}, `;
+      // this.cityDisplay.textContent = `${content.city}, `;
+      this.cityDisplay.textContent = `${content.city}`;
+      // this.provinceDisplay.textContent = `${content.province}, `;
+      this.provinceDisplay.textContent = `${content.province}`;
       this.countryDisplay.textContent = content.country;
       this.currentConditionIconDisplay.src = content.currentConditionIcon;
       this.currentConditionDisplay.textContent = content.currentCondition;
